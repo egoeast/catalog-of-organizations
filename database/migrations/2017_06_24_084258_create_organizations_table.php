cf6da5ae8c68c00fb2d03b054372ee1469de5ea4
Admin <das.east2mail.ru>
2017-06-24 21:28:35 +0300

    Изменение названий

---
+++ database/migrations/2017_06_24_084258_create_organizations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',200);
+            $table->string('img',200);
+            $table->string('adress',200);
+            $table->string('phone',100);
+            $table->text('description');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizations');
+    }
+}
